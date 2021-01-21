0ee8159f1197d2b8b55bb934505602714990d6a8
michihiro <pomeofishbird0303@gmail.com>
2021-01-21 18:49:03 +0900

    first commit

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -5,22 +5,24 @@ import CartContext from "context/CartContext";
 import  Badge  from "@material-ui/core/Badge"
 export function Cart() {
   const { checkout } = React.useContext(CartContext);
-  const totalQuantity = checkout?.lineItems.map(item => item.quantity)
-  const reducer = (sum, currentValue) => sum + currentValue
-  console.log(checkout)
-  // let totalQuantity = 0;
-  // if (checkout) {
-  //   checkout.lineItems.forEach(lineItem => {
-  //     totalQuantity = totalQuantity + lineItem.quantity;
-  //   })
-  // }
+  // const totalQuantity = checkout?.lineItems.map(item => item.quantity)
+  // const reducer = (sum, currentValue) => sum + currentValue
+  // console.log(checkout)
+  let totalQuantity = 0;
+  if (checkout) {
+    checkout.lineItems.forEach(lineItem => {
+      totalQuantity = totalQuantity + lineItem.quantity;
+    })
+  }
 
   return (
     <CartWrapper>
-      <Badge badgeContent={totalQuantity &&totalQuantity} color="secondary">
+      {/* <Badge badgeContent={totalQuantity &&totalQuantity} color="secondary"> */}
               <FaShoppingCart size="2.5rem" />
-      </Badge>
-
+      {/* </Badge> */}
+<div>
+        {totalQuantity} item(s) / £{checkout?.totalPrice || '0.00'}
+      </div>
       {/* <span>
         {totalQuantity.reduce(reducer) || 0}
      </span> */}
